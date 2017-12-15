e8c19b5e368ced343f38cd30a9b94ba7bd0f7d04
Nick Larson <nicklas.larson@gmail.com>
2017-12-15 13:33:06 -0600

    Day 2 - Problem 1 :white_check_mark:

--- 2017/02-corruption-checksum/index.js
+++ 2017/02-corruption-checksum/index.js
@@ -1,14 +1,11 @@
 const checksum = input => {
-  const diffs = input.map(entry => {
-    const array = entry
-      .toString()
-      .trim()
-      .split("")
-      .map(value => {
-        return parseInt(value);
-      });
-    return Math.max(...array) - Math.min(...array);
-  });
+  const diffs = input
+    .trim()
+    .split("\n")
+    .map(line => line.split(/\s+/g).map(value => parseInt(value)))
+    .map(array => {
+      return Math.max(...array) - Math.min(...array);
+    });
   return diffs.reduce((total, num) => total + num);
 };
 
--- 2017/02-corruption-checksum/index.test.js
+++ 2017/02-corruption-checksum/index.test.js
@@ -1,38 +1,11 @@
 import { checksum } from "./";
-import data from "./input.json";
+import data from "./input";
 
 describe("Day 2", () => {
   describe("Problem 1", () => {
     const inputs = [
-      ["sample input", [5195, 753, 2468], 18],
-      [
-        "zero sum input",
-        [1111, 111, 99999999, 888, 222, 22222222, 8888888888, 9999999, 99],
-        0
-      ],
-      [
-        "longer input",
-        [
-          1364,
-          461,
-          1438,
-          1456,
-          818,
-          999,
-          105,
-          1065,
-          314,
-          99,
-          1353,
-          148,
-          837,
-          590,
-          404,
-          123
-        ],
-        79
-      ],
-      ["puzzle input", data.input, 99]
+      ["sample input", `5 1 9 5\n7 5 3\n2 4 6 8`, 18],
+      ["puzzle input", data, 53460]
     ];
 
     inputs.forEach(([name, input, output]) => {
