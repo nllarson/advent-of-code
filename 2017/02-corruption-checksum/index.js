600a7bdeee97056f75d803b5abd9f373c98618ea
Nick Larson <nicklas.larson@gmail.com>
2017-12-17 18:22:17 -0600

    Day 2 - Problem 2 :white_check_mark:

--- 2017/02-corruption-checksum/index.js
+++ 2017/02-corruption-checksum/index.js
@@ -1,12 +1,31 @@
-const checksum = input => {
-  const diffs = input
+// For each line, determine the difference between the largest value and the smallest value
+const difference = line => {
+  return Math.max(...line) - Math.min(...line);
+};
+
+// For each line, find where the result of the division operation is a whole number, and divide them
+const quotient = line => {
+  return line
+    .map((number, index, array) => {
+      const divisor = array.find((elem, idx) => {
+        return index !== idx && number % elem === 0;
+      });
+
+      return (divisor !== undefined && number / divisor) || 0;
+    })
+    .reduce((total, num) => total + num);
+};
+
+const determineChecksum = (input, condenser) => {
+  const values = input
     .trim()
     .split("\n")
     .map(line => line.split(/\s+/g).map(value => parseInt(value)))
-    .map(array => {
-      return Math.max(...array) - Math.min(...array);
-    });
-  return diffs.reduce((total, num) => total + num);
+    .map(condenser);
+  return values.reduce((total, num) => total + num);
 };
 
-export { checksum };
+const checksum = input => determineChecksum(input, difference);
+const checksum2 = input => determineChecksum(input, quotient);
+
+export { checksum, checksum2 };
--- 2017/02-corruption-checksum/index.test.js
+++ 2017/02-corruption-checksum/index.test.js
@@ -1,4 +1,4 @@
-import { checksum } from "./";
+import { checksum, checksum2 } from "./";
 import data from "./input";
 
 describe("Day 2", () => {
@@ -11,6 +11,19 @@ describe("Day 2", () => {
     inputs.forEach(([name, input, output]) => {
       test(`${name} output should be ${output}`, () => {
         expect(checksum(input)).toBe(output);
+      });
+    });
+  });
+
+  describe("Problem 2", () => {
+    const inputs = [
+      ["sample input", `5 9 2 8\n9 4 7 3\n3 8 6 5`, 9],
+      ["puzzle input", data, 53460]
+    ];
+
+    inputs.forEach(([name, input, output]) => {
+      test(`${name} output should be ${output}`, () => {
+        expect(checksum2(input)).toBe(output);
       });
     });
   });
